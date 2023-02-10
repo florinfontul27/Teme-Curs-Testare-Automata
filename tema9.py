33a86fba58900970def4f38b5c2bab803e7216ed
florinfontul27 <121027191+florinfontul27@users.noreply.github.com>
2023-02-10 13:01:57 +0200

    s

---
+++ tema9.py
@@ -0,0 +1,10 @@
+'''http://automationpractice.com/index.php
+Alegeti cate 3 elemente din fiecare tip de selector din urmatoarele categorii:
+
+Id
+Link text
+Partial link text
+Name
+Tag
+Class name*
+'''
